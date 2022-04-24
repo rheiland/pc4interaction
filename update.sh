6d5546a5fb67dc28cc3fa086d3273c32960df63b
Randy Heiland <randy.heiland@gmail.com>
2022-04-24 13:38:13 -0400

    init

---
+++ update.sh
@@ -0,0 +1,9 @@
+#cp src/main.cpp src/main-bkup.cpp
+cp ~/git/PhysiCell-dev-paul/VERSION.txt src/.
+cp ~/git/PhysiCell-dev-paul/main.cpp src/.
+cp -R ~/git/PhysiCell-dev-paul/custom_modules src/
+cp -R ~/git/PhysiCell-dev-paul/core src/
+cp -R ~/git/PhysiCell-dev-paul/BioFVM src/
+cp -R ~/git/PhysiCell-dev-paul/modules src/
+cp ~/git/PhysiCell-dev-paul/config/PhysiCell_settings.xml config
+
